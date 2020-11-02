371335a4b4ac0989124a42eceac34a2fd822a7bc
kevin <justlike0728@gmail.com>
2020-11-03 00:52:36 +0800

    add edit create update html & controller action

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,8 +1,45 @@
 class CandidatesController < ApplicationController
 
 	def index
+		@candidates = Candidate.all
+	end
+
+	def show
+		@candidate = Candidate.find_by(id: params[:id])
 	end
 
 	def new
+		@candidate = Candidate.new
+	end
+
+	def create
+		@candidate = Candidate.new(candidate_params)
+
+		if @candidate.save
+			flash[:notice] = "Candidate Created!"
+			redirect_to '/candidates'
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@candidate = Candidate.find_by(id: params[:id])
+	end
+
+	def update
+		@candidate = Candidate.find_by(id: params[:id])
+
+		if @candidate.update(candidate_params)
+			flash[:notice] = "Candidate Update!"
+			redirect_to '/candidates'
+		else
+			render :edit
+		end
+	end
+
+	private
+	def candidate_params
+		params.require(:candidate).permit(:name, :party, :age, :politics)
 	end
 end
